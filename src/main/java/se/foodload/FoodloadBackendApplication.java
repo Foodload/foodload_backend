8766e7ea33693828850292db83baf66922201add
Gurk1992 <Gurk1992@users.noreply.github.com>
2020-06-10 19:12:38 +0200

    Setup

---
+++ src/main/java/se/foodload/FoodloadBackendApplication.java
@@ -0,0 +1,13 @@
+package se.foodload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodloadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodloadBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/foodload/FoodloadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.foodload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodloadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
