05e98e70c5297b8d03f4409504e783ebb4e9620a
Gurk1992 <Gurk1992@users.noreply.github.com>
2020-08-10 17:05:52 +0200

    fix

--- src/main/java/se/foodload/repository/ClientRepository.java
+++ src/main/java/se/foodload/repository/ClientRepository.java
@@ -10,7 +10,7 @@ import se.foodload.domain.Family;
 
 public interface ClientRepository extends JpaRepository<Client, Long> { //<Client, String>???
 	
-	public Optional<Client> findByClientId(String firebaseId);
+	public Optional<Client> findByFirebaseId(String firebaseId);
 
 	public Optional<Client> findByEmail(String email);
 	
