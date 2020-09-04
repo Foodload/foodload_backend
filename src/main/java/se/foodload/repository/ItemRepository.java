769451f3845e6b73f1dcc5bb9c777c9334dda0af
Gurk1992 <Gurk1992@users.noreply.github.com>
2020-09-05 01:41:32 +0200

    altered query again..

--- src/main/java/se/foodload/repository/ItemRepository.java
+++ src/main/java/se/foodload/repository/ItemRepository.java
@@ -21,7 +21,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 	@Transactional
 	// @Query(value = "SELECT * FROM item WHERE lower(Name) LIKE '%:pattern%' LIMIT
 	// 20 OFFSET :start", nativeQuery = true)
-	@Query(value = "SELECT * FROM item WHERE lower(Name) LIKE %?#{escape([0])} ?#{escapeCharacter()} LIMIT 20 OFFSET :start", nativeQuery = true)
+	@Query(value = "SELECT * FROM item WHERE lower(Name) LIKE %?#{escape(:pattern)} ?#{escapeCharacter()} LIMIT 20 OFFSET :start", nativeQuery = true)
 	public Optional<List<Item>> findMatchingItems(@Param("pattern") String pattern, @Param("start") int start);
 
 	public Optional<List<Item>> findFirst10ByNameStartingWithIgnoreCase(String name);
