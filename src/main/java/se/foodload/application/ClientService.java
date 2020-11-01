8e21c13a5b9fe83f108e5fa61e6d033131968ee6
Antonio <antonio_97@live.se>
2020-11-01 13:58:49 +0100

    interface package to lowercase

--- src/main/java/se/foodload/application/ClientService.java
+++ src/main/java/se/foodload/application/ClientService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import se.foodload.application.Interfaces.IClientService;
+import se.foodload.application.interfaces.IClientService;
 import se.foodload.application.exception.ClientNotFoundException;
 import se.foodload.domain.Client;
 import se.foodload.enums.ErrorEnums;
