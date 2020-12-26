a17150ab935ee6adede3bc7702cdf0ed733d84d3
Antonio <antonio_97@live.se>
2020-12-26 17:10:54 +0100

    rename for reuse

--- src/main/java/se/foodload/presentation/models/ItemCountModel.java
+++ src/main/java/se/foodload/presentation/models/ItemCountModel.java
@@ -5,12 +5,12 @@ import lombok.Data;
 import javax.validation.constraints.NotNull;
 
 @Data
-public class DeleteItemModel {
+public class ItemCountModel {
     @NotNull(message = "itemCountId missing")
     private long itemCountId;
 
     @NotNull(message = "amount for verification missing")
     private int amount;
 
-    public DeleteItemModel(){}
+    public ItemCountModel(){}
 }
